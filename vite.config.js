51231fce1a1d2bfed3c22c9dc757c632b2ce353b
Daniel Roe <daniel@roe.dev>
2022-08-17 16:19:18 +0200

    chore: migrate to pnpm (#137)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   test: {
+    deps: {
+      inline: ['@nuxt/test-utils-edge'],
+    },
     coverage: {
       reporter: ['text'],
     },
