2c87ea2fae10b1d0bc0a661684aeb645cad1fa79
Daniel Roe <daniel@roe.dev>
2022-08-17 13:29:37 +0200

    chore: migrate to pnpm

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,8 +1,7 @@
 import { defineNuxtConfig } from 'nuxt'
-import PartyTownModule from '..'
 
 export default defineNuxtConfig({
-  modules: [PartyTownModule],
+  modules: ['@nuxtjs/partytown'],
   app: {
     head: {
       script: [{ src: '/test-script.js', type: 'text/partytown' }],
